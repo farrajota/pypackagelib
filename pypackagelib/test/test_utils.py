f04a214324d9b62d5c0a2f7cb1f4f664005eba3a
farrajota <mglfarrajota@outlook.com>
2017-07-12 17:11:25 +0100

    Fix tests

--- pypackagelib/test/test_utils.py
+++ pypackagelib/test/test_utils.py
@@ -1,27 +1,27 @@
 import pytest
-from pypackagelib import utils
+import pypackagelib
 
 
 def test_sum():
     num1 = 10
     num2 = 5
-    res = utils.sum(num1, num2)
+    res = pypackagelib.utils.sum(num1, num2)
     assert(res == (num1 + num2))
 
 def test_sub():
     num1 = 10
     num2 = 5
-    res = utils.sub(num1, num2)
+    res = pypackagelib.utils.sub(num1, num2)
     assert(res == (num1 - num2))
 
-def test_sum():
+def test_mul():
     num1 = 10
     num2 = 5
-    res = utils.mul(num1, num2)
+    res = pypackagelib.utils.mul(num1, num2)
     assert(res == (num1 * num2))
 
-def test_sum():
+def test_div():
     num1 = 10
     num2 = 5
-    res = utils.div(num1, num2)
+    res = pypackagelib.utils.div(num1, num2)
     assert(res == (num1 / num2))
